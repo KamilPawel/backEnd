e93dcd046b6063792691a2cf85fa28e34f18d640
KamilPawel <ks6g18@soton.ac.uk>
2021-08-29 22:18:32 +0100

    Setting up the HTML template

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const expressLayouts = require('express-ejs-layout')
+const expressLayouts = require('express-ejs-layouts')
 
+const indexRouter = require('./routes/index')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -9,6 +10,8 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
+
+app.use('/', indexRouter)
 
 app.listen(process.env.PORT || 3000)
 
